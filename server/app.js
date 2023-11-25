5bd5afaf32389763b49256423f4fe8a8594849ef
VrankaM <matus.vranka72@gmail.com>
2023-11-25 17:32:40 +0100

    server boilerplate

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/test", (req, res) => {
+    console.log(req);
+    console.log(res);
+});
+
+app.listen(8080, (req, res) => {
+    console.log("serus")
+});
