b038f46daf5f899e24b8e18115623360c731c92f
VrankaM <matus.vranka72@gmail.com>
2023-11-26 01:43:11 +0100

    backend-update-1

--- backend/main.py
+++ backend/main.py
@@ -1,30 +1,33 @@
 from flask import Flask, request, jsonify 
 import requests
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def endpoint():
     return "Home"
 
 @app.get("/test")
-def getTest():
-    return "OK"
-
-@app.post("/test")
 def test():
     try:
-        data = request.get_json()
-        print(data)
+        data = request.args.get('data')
+        print("request arguments: ", data)
         if data:
-            response = requests.post("api", json={"key": "value"})
-            print(response)
-            return jsonify({'status': 'success', 'data': data})
+            return "aj data mam", 200
+            response = requests.post("", data)
+            if response:
+                print("AI response: ", response)
+                return jsonify({'status': 'success', 'data': response}), 200
+            else:
+                return jsonify({'status': 'error', 'message': 'Could not fetch response from AI'}), 400
         else:
-            return jsonify({'status': 'error', 'message': 'No JSON data found in the request body'}), 400
+            return "sicko ok", 200
+
     except Exception as e:
         return jsonify({'status': 'error', 'message': str(e)}), 500
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=8080, debug=True)
